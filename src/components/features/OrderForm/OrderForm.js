6a652f3c47946110d8db821faecc984d91714c67
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-15 19:39:00 +0200

    Create OrderForm component and implement it in Trip.js

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {Row, Col, Grid} from 'react-flexbox-grid';
+
+class OrderForm extends React.Component {
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            {/* <OrderSummary /> */}
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+export default OrderForm;
