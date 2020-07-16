6310bce90f0c68d2ccbd8c80a6528d3fbc83ad66
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-16 18:57:17 +0200

    Add js code for OrderOptionDropdown method

--- src/components/features/OrderOption/OrderOptionDropdown.js
+++ src/components/features/OrderOption/OrderOptionDropdown.js
@@ -1,8 +1,29 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const OrderOptionDropdown = () => (
-  <div>OrderOptionDropdown</div>
+import styles from './OrderOption.scss';
+import { formatPrice } from '../../../utils/formatPrice';
+
+const OrderOptionDropdown = ({values, required, currentValue, setOptionValue}) => (
+  <select
+    className={styles.dropdown}
+    value={currentValue}
+    onChange={event => setOptionValue(event.currentTarget.value)}
+  >
+    {required ? '' : (
+      <option key='null' value=''>---</option>
+    )}
+    {values.map(value => (
+      <option key={value.id} value={value.id}>{value.name} ({formatPrice(value.price)})</option>
+    ))}
+  </select>
 );
+
+OrderOptionDropdown.propTypes ={
+  currentValue: PropTypes.node,
+  setOptionValue: PropTypes.func,
+  values: PropTypes.array,
+  required: PropTypes.bool,
+};
 
 export default OrderOptionDropdown;
