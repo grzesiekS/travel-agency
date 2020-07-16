5ebaa1eea44572a38887838d767a00fc683e9025
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-16 17:45:43 +0200

    Implement OrderOption sub components

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -3,11 +3,32 @@ import PropTypes from 'prop-types';
 
 import styles from './OrderOption.scss';
 
-const OrderOption = props => (
-  <div className={styles.component}>
-    <h3 className={styles.title}>{props.name}</h3>
-  </div>
-);
+import OrderOptionDropdown from './OrderOptionDropdown';
+import OrderOptionIcons from './OrderOptionIcons';
+import OrderOptionCheckboxes from './OrderOptionCheckboxes';
+import OrderOptionNumber from './OrderOptionNumber';
+
+const optionTypes = {
+  dropdown: OrderOptionDropdown,
+  icons: OrderOptionIcons,
+  chackboxes: OrderOptionCheckboxes,
+  number: OrderOptionNumber,
+};
+
+const OrderOption = ({name, type, ...otherProps}) => {
+  const OptionComponent = optionTypes[type];
+
+  if(!OptionComponent){
+    return null;
+  } else {
+    return (
+      <div className={styles.component}>
+        <h3 className={styles.title}>{name}</h3>
+        <OptionComponent {...otherProps} />
+      </div>
+    );
+  }
+};
 
 OrderOption.propTypes = {
   name: PropTypes.string,
