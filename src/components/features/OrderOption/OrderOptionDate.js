90b30d5ffec19f132f2be9b2508776afcba64663
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-18 09:21:03 +0200

    Add react-datepicker to OrderOptionDate component

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import DatePicker from 'react-datepicker';
 
-const OrderOptionDate = () => (
-  <div>OrderOptionDate</div>
-);
+import styles from './OrderOption.scss';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+
+class OrderOptionDate extends React.Component {
+  tomorrowDate = () => {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    return tomorrow;
+  };
+
+  state = {
+    startDate: this.tomorrowDate(),
+  };
+
+  handleChange = date => {
+    this.setState({
+      startDate: date,
+    });
+    this.props.setOptionValue(date);
+  };
+
+  render() {
+
+    return (
+      <DatePicker
+        selected={this.state.startDate}
+        onChange={this.handleChange}
+        minDate={this.tomorrowDate()}
+        className={styles.input}
+      />
+    );
+  }
+}
+
+OrderOptionDate.propTypes = {
+  setOptionValue: PropTypes.func,
+};
 
 export default OrderOptionDate;
