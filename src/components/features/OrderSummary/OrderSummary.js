15d6f3436b6302c4c69b4a934c7ce355acafd3a9
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-20 18:04:38 +0200

    Add generateDuration function in OrderSummary.js

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -4,18 +4,48 @@ import PropTypes from 'prop-types';
 import styles from './OrderSummary.scss';
 import {calculateTotal} from '../../../utils/calculateTotal';
 import {formatPrice} from '../../../utils/formatPrice';
+import { formatDate } from '../../../utils/formatDate';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const OrderSummary = props => {
   return (
-    <h2 className={styles.component}>
+    <div>
+      <h2 className={styles.component}>
       Total: <strong>{formatPrice(calculateTotal(formatPrice(props.tripCost), props.options))}</strong>
-    </h2>
+      </h2>
+      {generateDuration(props.options['start-date'], props.tripDuration)}
+    </div>
   );
+};
+
+const generateDuration = (date, duration) => {
+  if(date != '') {
+    return (
+      <Grid>
+        <Row>
+          <Col md={2}>
+            <h2>From:</h2>
+            <h2>{formatDate(date)}</h2>
+          </Col>
+          <Col md={2}>
+            <h2>To:</h2>
+            <h2>{formatDate(date.setDate(date.getDate() + duration - 1))}</h2>
+            {returnToSelectedDate(date, duration)}
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+};
+
+const returnToSelectedDate = (date, duration) => {
+  date.setDate(date.getDate() - duration + 1);
 };
 
 OrderSummary.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
+  tripDuration: PropTypes.number,
 };
 
 export default OrderSummary;
