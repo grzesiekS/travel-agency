d985143d33c51bd7cded6b53de25d26c76f64912
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-23 20:11:36 +0200

    Add react alert package npm

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import './styles/global.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
+import { Provider as AlertProvider, positions, transitions} from 'react-alert';
 import store from './redux/store';
 import App from './App';
+import AlertTemplate from 'react-alert-template-basic';
+
+const alertOptions = {
+  position: positions.BOTTOM_CENTER,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE,
+};
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AlertProvider template={AlertTemplate} {...alertOptions}>
+      <App />
+    </AlertProvider>
   </Provider>,
   document.getElementById('app')
 );
