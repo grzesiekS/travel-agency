c899d86ebd75a22b16d14da99f05d91d7e5bf04a
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-23 20:59:16 +0200

    Add react-confirm-alert package

--- src/index.js
+++ src/index.js
@@ -2,23 +2,12 @@ import './styles/global.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import { Provider as AlertProvider, positions, transitions} from 'react-alert';
 import store from './redux/store';
 import App from './App';
-import AlertTemplate from 'react-alert-template-basic';
-
-const alertOptions = {
-  position: positions.BOTTOM_CENTER,
-  timeout: 5000,
-  offset: '30px',
-  transition: transitions.SCALE,
-};
 
 ReactDOM.render(
   <Provider store={store}>
-    <AlertProvider template={AlertTemplate} {...alertOptions}>
-      <App />
-    </AlertProvider>
+    <App />
   </Provider>,
   document.getElementById('app')
 );
