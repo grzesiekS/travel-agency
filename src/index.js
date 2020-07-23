f12eb066cba3e3399ad46e7e36a0291ed4847f30
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-23 21:41:01 +0200

    Add react alert in index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,22 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './redux/store';
 import App from './App';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
+
+const alertOptions = {
+  position: positions.MIDDLE,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE,
+};
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <AlertProvider template={AlertTemplate} {...alertOptions}>
+      <App />
+    </AlertProvider>
   </Provider>,
   document.getElementById('app')
 );
