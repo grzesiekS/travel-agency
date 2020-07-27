1bd88702f12f8314964544047e3dde0dd9243f7f
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-27 12:22:32 +0200

    Add getCountdownTime method in HappyHourAd component

--- src/components/features/HappyHourAd/HappyHourAd.test.js
+++ src/components/features/HappyHourAd/HappyHourAd.test.js
@@ -27,6 +27,39 @@ describe('Component HappyHourAd', () => {
   it('should render correct title name', () => {
     const component = shallow(<HappyHourAd {...mockProps} />);
     expect(component.find(select.title).text()).toEqual(mockProps.title);
-    expect(component.find(select.promoDescription).text()).toEqual(mockProps.promoDescription);
   });
+});
+
+const trueDate = Date;
+const mockDate = customDate => class extends Date {
+  constructor(...args) {
+    if(args.length) {
+      super(...args);
+    } else {
+      super(customDate);
+    }
+    return this;
+  }
+
+  static now() {
+    return (new Date(customDate)).getTime();
+  }
+};
+
+const checkDescriptionAtTime = (time, expectedDescription) => {
+  it('should show correct at 11:57:58', () => {
+    global.Date = mockDate(`2019-05-14T${time}.135Z`);
+
+    const component = shallow(<HappyHourAd {...mockProps} />);
+    const renderedTime = component.find(select.promoDescription).text();
+    expect(renderedTime).toEqual(expectedDescription);
+
+    global.Date = trueDate;
+  });
+};
+
+describe('Component HappyHourAd with mocked Date', () => {
+  checkDescriptionAtTime('11:57:58', '122');
+  checkDescriptionAtTime('11:59:59', '1');
+  checkDescriptionAtTime('13:00:00', 23 * 60 * 60 + '');
 });
