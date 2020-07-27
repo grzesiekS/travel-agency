539680325c1d66a7c57c54fc1c4847f8e1225e26
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-27 10:49:17 +0200

    Add dummy styles for title and promoDescription component in HappyHourAd.js

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import styles from './HappyHourAd.scss';
+
 class HappyHourAd extends React.Component {
   render() {
     return (
       <div>
-        <h3 className='title'></h3>
-        <div className='promoDescription'></div>
+        <h3 className={styles.title}></h3>
+        <div className={styles.promoDescription}></div>
       </div>
     );
   }
