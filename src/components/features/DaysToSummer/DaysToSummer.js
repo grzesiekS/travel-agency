4612f578cd5a0820be25968d34e84447e02414b9
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-28 20:17:21 +0200

    Add vacationCountDesc element in DaysToSummer component

--- src/components/features/DaysToSummer/DaysToSummer.js
+++ src/components/features/DaysToSummer/DaysToSummer.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import styles from './DaysToSummer.scss';
+
 class DaysToSummer extends React.Component {
   render() {
     return (
-      <div></div>
+      <div>
+        <h3 className={styles.vacationCountDesc}></h3>
+      </div>
     );
   }
 }
--- src/components/features/DaysToSummer/DaysToSummer.test.js
+++ src/components/features/DaysToSummer/DaysToSummer.test.js
@@ -2,10 +2,22 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import DaysToSummer from './DaysToSummer';
 
+const select = {
+  vacationCountDesc: '.vacationCountDesc',
+};
+
 describe('Component DaysToSummer', () => {
   it('should render without crashing', () => {
     const component = shallow(<DaysToSummer />);
 
     expect(component).toBeTruthy();
   });
+
+  it('should render vacationCountDesc element', () => {
+    const component = shallow(<DaysToSummer />);
+
+    expect(component.exists(select.vacationCountDesc)).toEqual(true);
+  });
 });
+
+
