53e281eedb1d4d4273b4c1f871d62bd9894efd95
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-07-28 15:40:07 +0200

    Add DaysToSummer component

---
+++ src/components/features/DaysToSummer/DaysToSummer.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import DaysToSummer from './DaysToSummer';
+
+describe('Component DaysToSummer', () => {
+  it('should render without crashing', () => {
+    const component = shallow(<DaysToSummer />);
+
+    expect(component).toBeTruthy();
+  });
+});
